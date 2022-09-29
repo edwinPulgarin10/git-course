589f47646ab790a983b874365b2b512032b16973
edwin <edwinPulgarin10>
2022-09-29 14:20:22 -0500

    Feature: Creando funcion que hace una sum con texto

--- src/app.py
+++ src/app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello World"
-    
+
+@app.route('/sum/<int:a>/<int:b>')
+def sum(a:int,b:int):
+    nums_sum = a+b
+    return str(nums_sum)
